4c894669fba6fd72f8fc4dd0d0b294d05c519b77
EduardoGPS-18 <eduardogps202@gmail.com>
2021-11-18 14:03:03 -0300

    ✅ : ensure http adapter throws bad request error on 400

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -3,6 +3,8 @@ import 'package:mockito/mockito.dart';
 import 'package:http/http.dart';
 import 'package:test/test.dart';
 
+import 'package:app_curso_manguinho/data/http/http.dart';
+
 import 'package:app_curso_manguinho/infra/http/http.dart';
 
 class ClientSpy extends Mock implements Client {}
@@ -96,6 +98,14 @@ void main() {
       final response = await sut.request(url: url, method: 'post');
 
       expect(response, null);
+    });
+
+    test('Should return BadRequestError if post returns 400', () async {
+      mockResponse(400);
+
+      final future = sut.request(url: url, method: 'post');
+
+      expect(future, throwsA(HttpError.badRequest));
     });
   });
 }
