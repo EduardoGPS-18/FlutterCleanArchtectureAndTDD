3a279ca51e2e6e863f7922aa6d6cb3aac2a5bc5c
EduardoGPS-18 <eduardogps202@gmail.com>
2021-11-18 12:51:10 -0300

    ✅ : ensure http adapter returns null if post returns 204 with data

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -27,7 +27,11 @@ class HttpAdapter {
       headers: headers,
       body: jsonBody,
     );
-    return response.body.isEmpty ? null : jsonDecode(response.body);
+    if (response.statusCode == 200) {
+      return response.body.isEmpty ? null : jsonDecode(response.body);
+    } else {
+      return null;
+    }
   }
 }
 
@@ -107,6 +111,14 @@ void main() {
     });
 
     test('Should return null if post returns 204', () async {
+      mockResponse(204, body: '');
+
+      final response = await sut.request(url: url, method: 'post');
+
+      expect(response, null);
+    });
+
+    test('Should return null if post returns 204 with data', () async {
       mockResponse(204, body: '');
 
       final response = await sut.request(url: url, method: 'post');
