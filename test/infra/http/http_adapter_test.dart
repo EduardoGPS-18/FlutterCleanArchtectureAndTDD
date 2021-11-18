d47d4414b4db287af6b81184021020ab35ac0449
EduardoGPS-18 <eduardogps202@gmail.com>
2021-11-18 13:58:33 -0300

    🛠️ : move http adapter to its own file

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -1,39 +1,11 @@
-import 'dart:convert';
-
 import 'package:faker/faker.dart';
 import 'package:mockito/mockito.dart';
 import 'package:http/http.dart';
 import 'package:test/test.dart';
-import 'package:meta/meta.dart';
+
+import 'package:app_curso_manguinho/infra/http/http.dart';
 
 class ClientSpy extends Mock implements Client {}
-
-class HttpAdapter {
-  final Client client;
-
-  HttpAdapter(this.client);
-  Future<Map> request({
-    @required String url,
-    @required String method,
-    Map body,
-  }) async {
-    final headers = {
-      'content-type': 'application/json',
-      'accept': 'application/json',
-    };
-    final jsonBody = body != null ? jsonEncode(body) : null;
-    final response = await client.post(
-      url,
-      headers: headers,
-      body: jsonBody,
-    );
-    if (response.statusCode == 200) {
-      return response.body.isEmpty ? null : jsonDecode(response.body);
-    } else {
-      return null;
-    }
-  }
-}
 
 void main() {
   Client client;
