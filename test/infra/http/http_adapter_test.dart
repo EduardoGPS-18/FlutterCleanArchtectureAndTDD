73ce88d6ef617df4a1b32b6ff013daa9df90c085
EduardoGPS-18 <eduardogps202@gmail.com>
2021-11-18 12:30:37 -0300

    ✅ : renamed file, moving definitions to setup and ensure default headers

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -11,20 +11,35 @@ class HttpAdapter {
 
   HttpAdapter(this.client);
   Future<void> request({@required String url, @required String method}) async {
-    await client.post(url);
+    final headers = {
+      'content-type': 'application/json',
+      'accept': 'application/json',
+    };
+    await client.post(url, headers: headers);
   }
 }
 
 void main() {
+  Client client;
+  HttpAdapter sut;
+  String url;
+
+  setUp(() {
+    client = ClientSpy();
+    sut = HttpAdapter(client);
+    url = faker.internet.httpUrl();
+  });
+
   group('post', () {
     test('Should call post with correct values', () async {
-      final client = ClientSpy();
-      final sut = HttpAdapter(client);
-      final url = faker.internet.httpUrl();
-
       await sut.request(url: url, method: 'post');
 
-      verify(client.post(url));
+      verify(
+        client.post(url, headers: {
+          'content-type': 'application/json',
+          'accept': 'application/json',
+        }),
+      );
     });
   });
 }
