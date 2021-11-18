cd7870dd8d43351f4aea167b867283726487ed01
EduardoGPS-18 <eduardogps202@gmail.com>
2021-11-18 14:17:33 -0300

    🛠️ : change control struct from if to switch

--- lib/infra/http/http_adapter.dart
+++ lib/infra/http/http_adapter.dart
@@ -27,14 +27,21 @@ class HttpAdapter implements HttpClient {
   }
 
   Map _handleResponse(Response response) {
-    if (response.statusCode == 200) {
-      return response.body.isEmpty ? null : jsonDecode(response.body);
-    } else if (response.statusCode == 204) {
-      return null;
-    } else if (response.statusCode == 400) {
-      throw HttpError.badRequest;
-    } else {
-      throw HttpError.serverError;
+    switch (response.statusCode) {
+      case 200:
+        return response.body.isEmpty ? null : jsonDecode(response.body);
+      case 204:
+        return null;
+      case 400:
+        throw HttpError.badRequest;
+      case 401:
+        throw HttpError.unauthorized;
+      case 403:
+        throw HttpError.forbidden;
+      case 404:
+        throw HttpError.notFound;
+      default:
+        throw HttpError.serverError;
     }
   }
 }
