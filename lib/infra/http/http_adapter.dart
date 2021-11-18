aba8479ca35438aa6c12a967770833fb5c573fa2
EduardoGPS-18 <eduardogps202@gmail.com>
2021-11-18 14:21:03 -0300

    ✅ : ensure http adapter return not found error on 404

--- lib/infra/http/http_adapter.dart
+++ lib/infra/http/http_adapter.dart
@@ -38,6 +38,8 @@ class HttpAdapter implements HttpClient {
         throw HttpError.unauthorized;
       case 403:
         throw HttpError.forbidden;
+      case 404:
+        throw HttpError.notFound;
       default:
         throw HttpError.serverError;
     }
