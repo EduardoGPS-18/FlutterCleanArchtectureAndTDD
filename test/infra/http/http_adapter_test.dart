86d68f6d67cbf2eeebc42113c47dd6f84c8ebcac
EduardoGPS-18 <eduardogps202@gmail.com>
2021-11-18 12:48:04 -0300

    🛠️ : move duplicated code to the setup

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -43,12 +43,26 @@ void main() {
   });
 
   group('post', () {
+    PostExpectation mockRequest() => when(client.post(
+          any,
+          body: anyNamed('body'),
+          headers: anyNamed('headers'),
+        ));
+
+    void mockResponse(
+      int statusCode, {
+      String body = '{"any_key":"any_value"}',
+    }) {
+      mockRequest().thenAnswer(
+        (_) async => Response(body, statusCode),
+      );
+    }
+
+    setUp(() {
+      mockResponse(200);
+    });
+
     test('Should call post with correct values', () async {
-      when(client.post(
-        any,
-        body: anyNamed('body'),
-        headers: anyNamed('headers'),
-      )).thenAnswer((_) async => Response('{"any_key":"any_value"}', 200));
       await sut.request(
         url: url,
         method: 'post',
@@ -68,11 +82,6 @@ void main() {
     });
 
     test('Should call post without body', () async {
-      when(client.post(
-        any,
-        body: anyNamed('body'),
-        headers: anyNamed('headers'),
-      )).thenAnswer((_) async => Response('{"any_key":"any_value"}', 200));
       await sut.request(url: url, method: 'post');
 
       verify(
@@ -84,17 +93,13 @@ void main() {
     });
 
     test('Should return data if post returns 200', () async {
-      when(client.post(any, headers: anyNamed('headers')))
-          .thenAnswer((_) async => Response('{"any_key":"any_value"}', 200));
-
       final response = await sut.request(url: url, method: 'post');
 
       expect(response, {'any_key': 'any_value'});
     });
 
     test('Should return null if post returns 200 with no data', () async {
-      when(client.post(any, headers: anyNamed('headers')))
-          .thenAnswer((_) async => Response('', 200));
+      mockResponse(200, body: '');
 
       final response = await sut.request(url: url, method: 'post');
 
