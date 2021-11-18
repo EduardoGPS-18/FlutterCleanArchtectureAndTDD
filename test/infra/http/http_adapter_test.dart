bf1ac0829611f1ad6c8b320e86f0feb1b4853245
EduardoGPS-18 <eduardogps202@gmail.com>
2021-11-18 14:25:54 -0300

    ✅ : ensure http adapter throws server error if invalid method is provided

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -20,6 +20,12 @@ void main() {
     url = faker.internet.httpUrl();
   });
 
+  group('shared', () {
+    test('Should throw ServerError if invalid method is provided', () async {
+      final future = sut.request(url: url, method: 'invalid_method');
+      expect(future, throwsA(HttpError.serverError));
+    });
+  });
   group('post', () {
     PostExpectation mockRequest() => when(client.post(
           any,
