ddbab194b126d40f6b69899ed25c1c5a2c9175ef
EduardoGPS-18 <eduardogps202@gmail.com>
2021-11-18 12:42:40 -0300

    ✅ : ensure http adapter returns null if post returns 200 with no data

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -27,7 +27,7 @@ class HttpAdapter {
       headers: headers,
       body: jsonBody,
     );
-    return jsonDecode(response.body);
+    return response.body.isEmpty ? null : jsonDecode(response.body);
   }
 }
 
@@ -66,6 +66,7 @@ void main() {
         ),
       );
     });
+
     test('Should call post without body', () async {
       when(client.post(
         any,
@@ -81,6 +82,7 @@ void main() {
         ),
       );
     });
+
     test('Should return data if post returns 200', () async {
       when(client.post(any, headers: anyNamed('headers')))
           .thenAnswer((_) async => Response('{"any_key":"any_value"}', 200));
@@ -88,6 +90,15 @@ void main() {
       final response = await sut.request(url: url, method: 'post');
 
       expect(response, {'any_key': 'any_value'});
+    });
+
+    test('Should return null if post returns 200 with no data', () async {
+      when(client.post(any, headers: anyNamed('headers')))
+          .thenAnswer((_) async => Response('', 200));
+
+      final response = await sut.request(url: url, method: 'post');
+
+      expect(response, null);
     });
   });
 }
