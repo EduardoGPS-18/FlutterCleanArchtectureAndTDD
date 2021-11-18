14362efb87145a5e32c8d3d110098272f1ab80b2
EduardoGPS-18 <eduardogps202@gmail.com>
2021-11-18 12:19:56 -0300

    ✅ : ensure http adapter calls post with corrent url

---
+++ test/infra/http/http_adapter_teste.dart
@@ -0,0 +1,30 @@
+import 'package:faker/faker.dart';
+import 'package:mockito/mockito.dart';
+import 'package:http/http.dart';
+import 'package:test/test.dart';
+import 'package:meta/meta.dart';
+
+class ClientSpy extends Mock implements Client {}
+
+class HttpAdapter {
+  final Client client;
+
+  HttpAdapter(this.client);
+  Future<void> request({@required String url, @required String method}) async {
+    await client.post(url);
+  }
+}
+
+void main(List<String> args) {
+  group('post', () {
+    test('Should call post with correct values', () async {
+      final client = ClientSpy();
+      final sut = HttpAdapter(client);
+      final url = faker.internet.httpUrl();
+
+      await sut.request(url: url, method: 'post');
+
+      verify(client.post(url));
+    });
+  });
+}
