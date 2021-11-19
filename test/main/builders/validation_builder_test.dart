707f0e461c8e4a7ebde46194254aa661d448bf41
EduardoGPS-18 <eduardogps202@gmail.com>
2021-11-19 18:45:40 -0300

    ✅ : ensure validation builder return a single field with some validators

---
+++ test/main/builders/validation_builder_test.dart
@@ -0,0 +1,19 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:app_curso_manguinho/main/builders/builders.dart';
+import 'package:app_curso_manguinho/validation/protocols/protocols.dart';
+import 'package:app_curso_manguinho/validation/validators/validators.dart';
+
+void main() {
+  test('validation builder ...', () {
+    test('Should return for a single field but some validator', () async {
+      final validations = ValidationComposite([
+        ...ValidationBuilder.field('any_field').required().email().build(),
+      ]);
+      expect(validations.validations, <FieldValidation>[
+        RequiredFieldValidation('any_field'),
+        EmailValidation('any_field'),
+      ]);
+    });
+  });
+}
