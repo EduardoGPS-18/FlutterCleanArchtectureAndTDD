c33bd513b883bcdd40dff3761212a9dc4647ac45
EduardoGPS-18 <eduardogps202@gmail.com>
2021-11-19 15:25:13 -0300

    ✅ : ensure required validation field return error if value is empty

--- test/validation/validators/required_field_validation_test.dart
+++ test/validation/validators/required_field_validation_test.dart
@@ -12,7 +12,7 @@ class RequiredFieldValidation implements FieldValidation {
 
   @override
   String validate(String value) {
-    return null;
+    return value.isNotEmpty ? null : 'Campo obrigatório';
   }
 }
 
@@ -23,5 +23,13 @@ void main() {
     final error = sut.validate('any_value');
 
     expect(error, null);
+  });
+
+  test('Should return error if value is empty', () {
+    final sut = RequiredFieldValidation('any');
+
+    final error = sut.validate('');
+
+    expect(error, 'Campo obrigatório');
   });
 }
