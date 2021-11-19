9740199ca141df9c0ad7ce043f230ce6b4f45199
EduardoGPS-18 <eduardogps202@gmail.com>
2021-11-19 18:43:57 -0300

    ✅ : ensure the make login validations test is returning correct validations

--- test/main/factories/pages/login/login_validation_factory_test.dart
+++ test/main/factories/pages/login/login_validation_factory_test.dart
@@ -1,11 +1,13 @@
+import 'package:app_curso_manguinho/main/builders/builders.dart';
 import 'package:app_curso_manguinho/main/factories/pages/login/login_validation_factory.dart';
+import 'package:app_curso_manguinho/validation/protocols/field_validation.dart';
 import 'package:app_curso_manguinho/validation/validators/validators.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   test('Should return the correct validations', () async {
     final validations = makeLoginValidations();
-    expect(validations, [
+    expect(validations, <FieldValidation>[
       RequiredFieldValidation('email'),
       EmailValidation('email'),
       RequiredFieldValidation('password'),
