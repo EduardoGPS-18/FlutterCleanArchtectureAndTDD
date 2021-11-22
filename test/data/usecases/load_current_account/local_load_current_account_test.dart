5b0d3c39d5697b7594fbb908036262e678350b55
EduardoGPS-18 <eduardogps202@gmail.com>
2021-11-22 13:58:20 -0300

    ✅ : ensure local load current account throws unexpected error if fetch secure cached storage throws

--- test/data/usecases/load_current_account/local_load_current_account_test.dart
+++ test/data/usecases/load_current_account/local_load_current_account_test.dart
@@ -1,4 +1,5 @@
 import 'package:app_curso_manguinho/domain/entities/account_entity.dart';
+import 'package:app_curso_manguinho/domain/helpers/domain_error.dart';
 import 'package:faker/faker.dart';
 import 'package:meta/meta.dart';
 import 'package:test/test.dart';
@@ -14,8 +15,12 @@ class LocalLoadCurrentAccount implements LoadCurrentAccount {
   });
 
   Future<AccountEntity> load() async {
-    final token = await fetchSecureCacheStorage.fetchSecure('token');
-    return AccountEntity(token: token);
+    try {
+      final token = await fetchSecureCacheStorage.fetchSecure('token');
+      return AccountEntity(token: token);
+    } on Exception {
+      throw DomainError.unexpected;
+    }
   }
 }
 
@@ -32,6 +37,7 @@ void main() {
 
   PostExpectation mockFetchSecureCall() => when(fetchSecureCacheStorage.fetchSecure(any));
   void mockFetchSecureSuccess() => mockFetchSecureCall().thenAnswer((_) async => token);
+  void mockFetchSecureError() => mockFetchSecureCall().thenThrow(Exception());
 
   setUp(() {
     fetchSecureCacheStorage = FetchSecureCacheStorageSpy();
@@ -51,5 +57,13 @@ void main() {
     final account = await sut.load();
 
     expect(account, AccountEntity(token: token));
+  });
+
+  test('Should throw UnexpectedError if FetchSecureCachedStorage throws', () async {
+    mockFetchSecureError();
+
+    final future = sut.load();
+
+    expect(future, throwsA(DomainError.unexpected));
   });
 }
