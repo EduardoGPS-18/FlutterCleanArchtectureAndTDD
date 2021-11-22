9c70bd2cfcfebdd1e8fdec9b4c3cd5244e790f15
EduardoGPS-18 <eduardogps202@gmail.com>
2021-11-22 11:52:49 -0300

    🔥 : remove stream login presenter

--- lib/main/factories/pages/login/login_presenter_factory.dart
+++ lib/main/factories/pages/login/login_presenter_factory.dart
@@ -5,11 +5,6 @@ import '../../../../ui/pages/login/login.dart';
 import '../../../../main/factories/pages/login/login.dart';
 import '../../../../main/factories/usecases/usecases.dart';
 
-LoginPresenter makeStreamLoginPresenter() => StreamLoginPresenter(
-      validation: makeValidationComposite(),
-      authenticationUsecase: makeRemoteAuthentication(),
-    );
-
 LoginPresenter makeGetxLoginPresenter() => GetxLoginPresenter(
       validation: makeValidationComposite(),
       authenticationUsecase: makeRemoteAuthentication(),
