5bcc19a88f805592299c8eb06b561fbcc7ce9431
EduardoGPS-18 <eduardogps202@gmail.com>
2021-11-22 15:01:05 -0300

    ✅ : ensure login page will not navigate to non existentent page

--- test/ui/pages/login_page_test.dart
+++ test/ui/pages/login_page_test.dart
@@ -289,4 +289,16 @@ void main() {
       expect(find.text('fake page'), findsOneWidget);
     },
   );
+
+  testWidgets('Should not change page', (WidgetTester tester) async {
+    await loadPage(tester);
+
+    navigateToController.add('');
+    await tester.pump();
+    expect(Get.currentRoute, '/login');
+
+    navigateToController.add(null);
+    await tester.pump();
+    expect(Get.currentRoute, '/login');
+  });
 }
