51173f97def31c36d5f39558fe59aa87726b6d5f
EduardoGPS-18 <eduardogps202@gmail.com>
2021-11-23 08:33:45 -0300

    🧩 : added use account usecase

---
+++ lib/domain/usecases/add_account.dart
@@ -0,0 +1,25 @@
+import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
+
+import '../entities/entities.dart';
+
+abstract class AddAccount {
+  Future<AccountEntity> auth({@required AddAccountParams params});
+}
+
+class AddAccountParams extends Equatable {
+  final String name;
+  final String email;
+  final String password;
+  final String passwordConfirmation;
+
+  AddAccountParams({
+    @required this.name,
+    @required this.email,
+    @required this.password,
+    @required this.passwordConfirmation,
+  });
+
+  @override
+  List<Object> get props => [name, email, password, passwordConfirmation];
+}
