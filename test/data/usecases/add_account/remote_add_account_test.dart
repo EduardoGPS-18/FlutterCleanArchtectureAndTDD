1a4b26effa1236d6f121c4a28531067ac5d85915
EduardoGPS-18 <eduardogps202@gmail.com>
2021-11-23 08:55:56 -0300

    ✅ : ensure remote add account throws unexpected error on 404

--- test/data/usecases/add_account/remote_add_account_test.dart
+++ test/data/usecases/add_account/remote_add_account_test.dart
@@ -105,4 +105,12 @@ void main() {
 
     expect(future, throwsA(DomainError.unexpected));
   });
+
+  test('Should throw UnexpectedError if HttpClient returns 404', () async {
+    mockHttpError(HttpError.notFound);
+
+    final future = sut.add(params: params);
+
+    expect(future, throwsA(DomainError.unexpected));
+  });
 }
