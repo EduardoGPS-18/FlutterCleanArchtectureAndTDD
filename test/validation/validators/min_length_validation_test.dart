c8ba303de538dcbd7e9d8eaf0e19f47a2bd69445
EduardoGPS-18 <eduardogps202@gmail.com>
2021-11-23 15:45:34 -0300

    ✅ : ensure min length validation returns null if value is bigger then min size

--- test/validation/validators/min_length_validation_test.dart
+++ test/validation/validators/min_length_validation_test.dart
@@ -14,7 +14,7 @@ class MinLengthValidation implements FieldValidation {
 
   @override
   ValidationError validate(String value) {
-    return value != null && value.length == size ? null : ValidationError.invalidField;
+    return value != null && value.length >= size ? null : ValidationError.invalidField;
   }
 }
 
@@ -39,5 +39,9 @@ void main() {
 
   test('Should return null if value is equal than min size', () {
     expect(sut.validate(faker.randomGenerator.string(5, min: 5)), null);
+  });
+
+  test('Should return null if value is bigger than min size', () {
+    expect(sut.validate(faker.randomGenerator.string(10, min: 6)), null);
   });
 }
