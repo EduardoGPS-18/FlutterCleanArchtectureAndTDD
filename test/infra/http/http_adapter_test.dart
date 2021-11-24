105fc594cbbbb4c09c9382148ce5a28e90b5ecab
EduardoGPS-18 <eduardogps202@gmail.com>
2021-11-24 11:47:52 -0300

    ✅ : ensure http adapter throws not found on 404

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -268,5 +268,13 @@ void main() {
 
       expect(future, throwsA(HttpError.forbidden));
     });
+
+    test('Should return ForbiddenError if get returns 404', () async {
+      mockResponse(404);
+
+      final future = sut.request(url: url, method: 'get');
+
+      expect(future, throwsA(HttpError.notFound));
+    });
   });
 }
