507cfd718fe35e4fdcd83260817bb7819d7c819f
EduardoGPS-18 <eduardogps202@gmail.com>
2021-11-24 11:51:36 -0300

    ✅ : ensure http adapter throws server error if get throws

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -181,6 +181,8 @@ void main() {
       );
     }
 
+    void mockError() => mockRequest().thenThrow(Exception());
+
     setUp(() {
       mockResponse(200);
     });
@@ -279,6 +281,14 @@ void main() {
 
     test('Should return ServerError if get returns 500', () async {
       mockResponse(500);
+
+      final future = sut.request(url: url, method: 'get');
+
+      expect(future, throwsA(HttpError.serverError));
+    });
+
+    test('Should return ServerError if get throws', () async {
+      mockError();
 
       final future = sut.request(url: url, method: 'get');
 
