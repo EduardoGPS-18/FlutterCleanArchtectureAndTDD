c823da030d654747f54e60f7ed5bd523844148ef
EduardoGPS-18 <eduardogps202@gmail.com>
2021-11-24 11:44:13 -0300

    ✅ : ensure http adapter throws bad request on 400

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -236,5 +236,21 @@ void main() {
 
       expect(response, isNull);
     });
+
+    test('Should return BadRequestError if get returns 400', () async {
+      mockResponse(400);
+
+      final future = sut.request(url: url, method: 'get');
+
+      expect(future, throwsA(HttpError.badRequest));
+    });
+
+    test('Should return BadRequestError if get returns 400', () async {
+      mockResponse(400, body: '');
+
+      final future = sut.request(url: url, method: 'get');
+
+      expect(future, throwsA(HttpError.badRequest));
+    });
   });
 }
