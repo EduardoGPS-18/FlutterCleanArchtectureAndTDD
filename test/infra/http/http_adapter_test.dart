acf53da95cc4b65712917e1ec8daf30db9e7caec
EduardoGPS-18 <eduardogps202@gmail.com>
2021-11-24 11:43:12 -0300

    ✅ : ensure http adapter returns bad request if post returns 400 with no data

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -117,6 +117,15 @@ void main() {
 
       expect(future, throwsA(HttpError.badRequest));
     });
+
+    test('Should return BadRequestError if post returns 400', () async {
+      mockResponse(400, body: '');
+
+      final future = sut.request(url: url, method: 'post');
+
+      expect(future, throwsA(HttpError.badRequest));
+    });
+
     test('Should return UnauthorizedError if post returns 401', () async {
       mockResponse(401);
 
