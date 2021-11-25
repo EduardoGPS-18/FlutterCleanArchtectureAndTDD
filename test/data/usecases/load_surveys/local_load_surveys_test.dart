47b83dc0f8373314a2c06e8f52c2551ce4e28bf7
EduardoGPS-18 <eduardogps202@gmail.com>
2021-11-25 13:14:21 -0300

    ✅ : ensure local load surveys delete cache if it is incomplete

--- test/data/usecases/load_surveys/local_load_surveys_test.dart
+++ test/data/usecases/load_surveys/local_load_surveys_test.dart
@@ -167,5 +167,17 @@ void main() {
 
       verify(cacheStorage.delete('surveys')).called(1);
     });
+
+    test('Should delete cache if it is incomplete', () async {
+      mockFetch([
+        {
+          'date': '2020-04-27T00:00:00Z',
+          'didAnswer': "true",
+        }
+      ]);
+      await sut.validate();
+
+      verify(cacheStorage.delete('surveys')).called(1);
+    });
   });
 }
