5b1be99b6d390f64986644d02886a91d8ae4c022
EduardoGPS-18 <eduardogps202@gmail.com>
2021-11-25 13:13:11 -0300

    🛠️ : move duplicated code to a helper method

--- lib/data/usecases/load_surveys/local_load_surveys.dart
+++ lib/data/usecases/load_surveys/local_load_surveys.dart
@@ -20,7 +20,7 @@ class LocalLoadSurveys implements LoadSurveys {
       if (data?.isEmpty != false) {
         throw Exception();
       }
-      return data.map<SurveyEntity>((json) => LocalSurveyModel.fromJson(json).toEntity()).toList();
+      return _map(data);
     } catch (error) {
       throw DomainError.unexpected;
     }
@@ -29,9 +29,13 @@ class LocalLoadSurveys implements LoadSurveys {
   Future<void> validate() async {
     final data = await cacheStorage.fetch('surveys');
     try {
-      data.map<SurveyEntity>((json) => LocalSurveyModel.fromJson(json).toEntity()).toList();
+      _map(data);
     } catch (_) {
       await cacheStorage.delete('surveys');
     }
+  }
+
+  List<SurveyEntity> _map(List<Map> list) {
+    list.map<SurveyEntity>((json) => LocalSurveyModel.fromJson(json).toEntity()).toList();
   }
 }
