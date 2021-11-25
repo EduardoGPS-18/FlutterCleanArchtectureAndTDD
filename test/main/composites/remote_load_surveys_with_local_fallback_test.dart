9800ae460091a0bb243069496d8ffe014b29e4f5
EduardoGPS-18 <eduardogps202@gmail.com>
2021-11-25 16:20:26 -0300

    ✅ : ensure composite calls local save with remote data

--- test/main/composites/remote_load_surveys_with_local_fallback_test.dart
+++ test/main/composites/remote_load_surveys_with_local_fallback_test.dart
@@ -1,35 +1,70 @@
 import 'package:mockito/mockito.dart';
+import 'package:faker/faker.dart';
 import 'package:meta/meta.dart';
 import 'package:test/test.dart';
+
+import 'package:app_curso_manguinho/domain/entities/entities.dart';
 
 import 'package:app_curso_manguinho/data/usecases/load_surveys/load_surveys.dart';
 
 class RemoteLoadSurveysWithLocalFallback {
   final RemoteLoadSurveys remote;
+  final LocalLoadSurveys local;
 
   RemoteLoadSurveysWithLocalFallback({
     @required this.remote,
+    @required this.local,
   });
 
   Future<void> load() async {
-    await remote.load();
+    final surveys = await remote.load();
+    await local.save(surveys);
   }
 }
 
 class RemoteLoadSurveysSpy extends Mock implements RemoteLoadSurveys {}
 
+class LocalLoadSurveysSpy extends Mock implements LocalLoadSurveys {}
+
 void main() {
   RemoteLoadSurveysWithLocalFallback sut;
   RemoteLoadSurveys remote;
+  LocalLoadSurveys local;
+  List<SurveyEntity> surveys;
+
+  List<SurveyEntity> mockSurveys() => [
+        SurveyEntity(
+          id: faker.guid.guid(),
+          question: faker.randomGenerator.string(50),
+          dateTime: faker.date.dateTime(),
+          didAnswer: faker.randomGenerator.boolean(),
+        ),
+      ];
+
+  void mockRemoteLoad() {
+    surveys = mockSurveys();
+    when(remote.load()).thenAnswer((_) async => surveys);
+  }
 
   setUp(() {
     remote = RemoteLoadSurveysSpy();
-    sut = RemoteLoadSurveysWithLocalFallback(remote: remote);
+    local = LocalLoadSurveysSpy();
+    sut = RemoteLoadSurveysWithLocalFallback(
+      remote: remote,
+      local: local,
+    );
+    mockRemoteLoad();
   });
 
   test('Should call remote load', () async {
     await sut.load();
 
     verify(remote.load()).called(1);
+  });
+
+  test('Should call local save with remote data', () async {
+    await sut.load();
+
+    verify(local.save(surveys)).called(1);
   });
 }
