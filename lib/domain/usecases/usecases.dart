a0e2b9caafa15896c0ed0dc184ba973cc1dfa5c3
EduardoGPS-18 <eduardogps202@gmail.com>
2021-11-26 08:36:41 -0300

    🧩 : added load survey result

--- lib/domain/usecases/usecases.dart
+++ lib/domain/usecases/usecases.dart
@@ -1,5 +1,6 @@
 export 'save_current_account.dart';
 export 'load_current_account.dart';
+export 'load_surveys_result.dart';
 export 'authentication.dart';
 export 'load_surveys.dart';
 export 'add_account.dart';
