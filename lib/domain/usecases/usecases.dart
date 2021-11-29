2fe4d59dba9e27aa5a2c2e7b40b26779c96e6a4d
EduardoGPS-18 <eduardogps202@gmail.com>
2021-11-29 14:10:59 -0300

    🧩 : save survey result usecase

--- lib/domain/usecases/usecases.dart
+++ lib/domain/usecases/usecases.dart
@@ -1,6 +1,7 @@
 export 'save_current_account.dart';
 export 'load_current_account.dart';
 export 'load_surveys_result.dart';
+export 'save_surveys_result.dart';
 export 'authentication.dart';
 export 'load_surveys.dart';
 export 'add_account.dart';
