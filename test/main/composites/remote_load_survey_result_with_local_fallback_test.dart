1367f1aded008e186edbe43e8ad1c9babe3cb429
EduardoGPS-18 <eduardogps202@gmail.com>
2021-11-29 11:44:30 -0300

    ✅ : ensure composite returns remote data

--- test/main/composites/remote_load_survey_result_with_local_fallback_test.dart
+++ test/main/composites/remote_load_survey_result_with_local_fallback_test.dart
@@ -1,3 +1,4 @@
+import 'package:app_curso_manguinho/domain/usecases/load_surveys_result.dart';
 import 'package:mockito/mockito.dart';
 import 'package:faker/faker.dart';
 import 'package:meta/meta.dart';
@@ -7,7 +8,7 @@ import 'package:app_curso_manguinho/data/usecases/usecases.dart';
 import 'package:app_curso_manguinho/domain/entities/entities.dart';
 import 'package:app_curso_manguinho/data/usecases/load_survey_result/local_load_survey_result.dart';
 
-class RemoteLoadSurveyResultWithLocalFallback {
+class RemoteLoadSurveyResultWithLocalFallback implements LoadSurveyResult {
   final RemoteLoadSurveyResult remote;
   final LocalLoadSurveyResult local;
 
@@ -16,9 +17,10 @@ class RemoteLoadSurveyResultWithLocalFallback {
     @required this.local,
   });
 
-  Future<void> loadBySurvey({String surveyId}) async {
+  Future<SurveyResultEntity> loadBySurvey({String surveyId}) async {
     final res = await remote.loadBySurvey(surveyId: surveyId);
     await local.save(surveyId: surveyId, surveyResult: res);
+    return res;
   }
 }
 
@@ -72,5 +74,11 @@ void main() {
     await sut.loadBySurvey(surveyId: surveyId);
 
     verify(local.save(surveyId: surveyId, surveyResult: surveyResult)).called(1);
+  });
+
+  test('Should return remote data', () async {
+    final response = await sut.loadBySurvey(surveyId: surveyId);
+
+    expect(response, surveyResult);
   });
 }
